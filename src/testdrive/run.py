fd21a54d923945d180ed8dab25f6b898fd3c93aa
David Spence <dspence@redhat.com>
2023-08-25 08:09:11 +0100

    Take lines of JSON, not JSON files.

--- src/testdrive/run.py
+++ src/testdrive/run.py
@@ -8,12 +8,18 @@ import os
 import subprocess
 from datetime import (datetime, timezone)
 
+from .common import open_input
 from .source import (Source, sequence)
 from .uri import UriBuilder
 
 def drive(test, *test_args):
     """Execute `test` and return a result dict"""
-    subp = subprocess.run([test] + list(test_args), capture_output=True, check=False, env=os.environ)
+    subp = subprocess.run(
+        [test] + list(test_args),
+        capture_output=True,
+        check=False,
+        env=os.environ,
+    )
     if not subp.returncode and not subp.stderr:
         return json.loads(subp.stdout)
     reason = f'{test} exited with code {subp.returncode}'
@@ -41,7 +47,7 @@ def main():
         '--basedir',
         help=' '.join((
             "The base directory which tests are relative to.",
-            "If not supplied tests are relative to the directory of filename.",
+            "If not supplied tests are relative to directory of `input`.",
         )),
     )
     aparser.add_argument(
@@ -53,25 +59,31 @@ def main():
         help="The base URL which tests are relative to.",
     )
     aparser.add_argument(
-        'filename',
-        help="File containing JSON-encoded sequence of tests to run.",
+        'input',
+        help=' '.join((
+            "File containing tests to run or '-' to read from stdin.",
+            "Each test is specified on a separate line as a JSON array.",
+            "The first element is the name of the test implementation,",
+            "relative to `--basedir`.",
+            "The remaining elements are args to the test implementation.",
+        )),
     )
     args = aparser.parse_args()
-    basedir = args.basedir or os.path.dirname(args.filename)
+    basedir = args.basedir or os.path.dirname(args.input)
     builder = UriBuilder(args.baseurl)
-    with open(args.filename, encoding='utf-8') as fid:
-        source = Source(sequence(json.load(fid)))
-    for test, *test_args in source.next():
-        if not args.timeless:
-            start = timenow()
-        result = drive(os.path.join(basedir, test), *test_args)
-        if not args.timeless:
-            end = timenow()
-        result['id'] = builder.build(os.path.dirname(test))
-        if not args.timeless:
-            result['timestamp'] = timestamp(start)
-            result['time'] = (end - start).total_seconds()
-        print(json.dumps(result))
+    with open_input(args.input) as fid:
+        source = Source(sequence(json.loads(line) for line in fid))
+        for test, *test_args in source.next():
+            if not args.timeless:
+                start = timenow()
+            result = drive(os.path.join(basedir, test), *test_args)
+            if not args.timeless:
+                end = timenow()
+            result['id'] = builder.build(os.path.dirname(test))
+            if not args.timeless:
+                result['timestamp'] = timestamp(start)
+                result['time'] = (end - start).total_seconds()
+            print(json.dumps(result))
 
 if __name__ == '__main__':
     main()
