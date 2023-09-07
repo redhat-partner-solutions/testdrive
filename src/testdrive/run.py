8b9d2fa8915b0a2f76ef740a54f618c2fd615258
David Spence <35847838+spenced@users.noreply.github.com>
2023-09-07 12:37:25 +0100

    Optionally plot images for a test implementation. (#10)
    Co-authored-by: David Spence <dspence@redhat.com>

--- src/testdrive/run.py
+++ src/testdrive/run.py
@@ -42,6 +42,31 @@ def drive(test, *test_args):
     dct['argv'] = test_args
     return dct
 
+def plot(plotter, prefix, *test_args):
+    """Execute `plotter` and return a sequence of images output.
+
+    If `plotter` exits with error or outputs to stderr, then raise RuntimeError.
+
+    Otherwise the sequence of images output contains whatever `plotter` outputs
+    to stdout. This output is always expected to be a JSON array whose items are
+    either a string, the path to an image filename, or an object with pairs for
+    'path', the path to an image filename, and optionally a string 'title'. Each
+    image output by `plotter` is expected to use `prefix` as the path and stem
+    for the output filename.
+    """
+    subp = subprocess.run(
+        (plotter, prefix) + test_args,
+        capture_output=True,
+        check=False,
+        env=os.environ,
+    )
+    if not subp.returncode and not subp.stderr:
+        return json.loads(subp.stdout)
+    reason = f'{plotter} exited with code {subp.returncode}:'
+    reason += '\n\n'
+    reason += subp.stderr.decode()
+    raise RuntimeError(reason)
+
 def timenow():
     """Return a datetime value for UTC time now."""
     return datetime.now(timezone.utc)
@@ -62,6 +87,23 @@ def main():
         help=' '.join((
             "The base directory which tests are relative to.",
             "If not supplied tests are relative to directory of `input`.",
+        )),
+    )
+    aparser.add_argument(
+        '--imagedir',
+        help=' '.join((
+            "The directory which plot image files are to be generated in.",
+            "If not supplied then no plots are generated.",
+        )),
+    )
+    aparser.add_argument(
+        '--plotter', default='plot.py',
+        help=' '.join((
+            "Generate a plot of test data by calling a script with this name.",
+            "The script must be colocated with the test implementation.",
+            "The args to the script are the output filename, followed by the",
+            "same argv as passed to the test implementation.",
+            "Ignored if plots are not generated.",
         )),
     )
     aparser.add_argument(
@@ -88,15 +130,25 @@ def main():
     with open_input(args.input) as fid:
         source = Source(sequence(json.loads(line) for line in fid))
         for test, *test_args in source.next():
+            id_ = builder.build(os.path.dirname(test))
+            testimpl = os.path.join(basedir, test)
             if not args.timeless:
                 start = timenow()
-            result = drive(os.path.join(basedir, test), *test_args)
+            result = drive(testimpl, *test_args)
             if not args.timeless:
                 end = timenow()
-            result['id'] = builder.build(os.path.dirname(test))
+            result['id'] = id_
             if not args.timeless:
                 result['timestamp'] = timestamp(start)
                 result['time'] = (end - start).total_seconds()
+            if result['result'] in (True, False) and args.imagedir:
+                plotter = os.path.join(os.path.dirname(testimpl), args.plotter)
+                if os.path.isfile(plotter):
+                    prefix = os.path.join(
+                        args.imagedir,
+                        os.path.splitext(test)[0].strip('/').replace('/', '_'),
+                    )
+                    result['plot'] = plot(plotter, prefix, *test_args)
             # Python exits with error code 1 on EPIPE
             if not print_line(json.dumps(result)):
                 sys.exit(1)
